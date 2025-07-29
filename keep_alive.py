9cc6d147b591a11bee5849147be6ab86341e675b
ArtGavin <162838339+ArtGavin@users.noreply.github.com>
2025-07-29 21:07:37 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,11 @@
+# keep_alive.py
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "✅ Bot is running!"
+def index():
+    return "Bot is alive!"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)  # Render ใช้ PORT จาก env ด้วยจะดีที่สุด
